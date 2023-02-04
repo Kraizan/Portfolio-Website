2efcea12ba86663c819ba0d80925053ddb4c9ec1
Vanshaj Bhatnagar <vanshajbhatnagar10@gmail.com>
2023-02-04 21:41:41 +0530

    navbar for lg-screen

---
+++ components/Navbar.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  const NavOptions = [
+    { Event: "About", Link: "#About" },
+    { Event: "Skills", Link: "#Skills" },
+    { Event: "Projects", Link: "#Projects" },
+    { Event: "Contact", Link: "#Contact" },
+  ];
+
+  return (
+    <nav className="flex flex-row justify-between px-40 pt-16 pb-8">
+      <div className="text-3xl my-auto">
+        <Link href="#">Portfolio</Link>
+      </div>
+      <div className="flex text-lg">
+        <div className="flex">
+          {NavOptions.map((NavOptions) => (
+            <div key={NavOptions.Event} className="mx-3 my-auto">
+              <Link href={NavOptions.Link}>
+                <div className="link-underline">{NavOptions.Event}</div>
+              </Link>
+            </div>
+          ))}
+        </div>
+        <div>
+          <button className="ml-4 tracking-widest resume resume-text p-2">
+            Resume
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
